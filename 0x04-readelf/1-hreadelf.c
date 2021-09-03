4621258db2860a3203f38df87623df0143489f09
ChristianCampos-55 <gamochris@gmail.com>
2021-09-02 21:22:31 -0500

    [ADD]Required tasks tested, not made files - advanced is nutbreakerish

---
+++ 0x04-readelf/1-hreadelf.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - main function - prints header section of ELF file
+ * @argc: argument counter
+ * @argv: argument vector
+ * @env:  environment variable
+ * Return: 0 or -1 upon failure
+ */
+int main(int argc, char **argv, char **env)
+{
+	char *act_args[] = {"/usr/bin/readelf", "-W", "-S", "", NULL};
+
+	if (argc != 2)
+		exit(0);
+
+	act_args[3] = argv[1];
+	if (execve(act_args[0], act_args, env) == -1)
+	{
+		perror("execv");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
---
+++ 0x04-readelf/2-hreadelf.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - in function, prints elf file header
+ * @argc: argument counter
+ * @argv: argument vector
+ * @env:  environment variable
+ * Return: 0 or -1 upon failure
+ */
+
+int main(int argc, char **argv, char **env)
+{
+	char *act_args[] = {"/usr/bin/readelf", "-W", "-l", "", NULL};
+
+	if (argc != 2)
+		exit(0);
+
+	act_args[3] = argv[1];
+	if (execve(act_args[0], act_args, env) == -1)
+	{
+		perror("execv");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
