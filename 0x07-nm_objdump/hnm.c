dd6373e60b9f518ac312e870b317bc0f8d6d1842
ChristianCampos-55 <gamochris@gmail.com>
2021-10-26 20:28:18 -0500

    [ADD]Project folder + tsk 0

---
+++ 0x07-nm_objdump/hnm.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - emulates nm command from GNU
+ * @argc: command line args
+ * @argv: vector of argc's
+ * @env: shell environment
+ * Return: EXIT_SUCCESS on success or EXIT_FAILURE otherwise
+ */
+
+int main(int argc, char **argv, char **env)
+{
+	char *hnmv[] = {"./hnm", "-p", "", NULL};
+
+	(void)argc;
+	hnmv[2] = argv[1];
+	if (execve("/usr/bin/nm", hnmv, env) == -1)
+	{
+		perror("execv");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
